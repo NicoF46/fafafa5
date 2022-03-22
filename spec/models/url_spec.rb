08f338b99d24eb2313f751f2cfddc8a63813cce5
nico <pfunes@fi.uba.ar>
2022-03-22 20:18:02 -0300

    fafa

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -12,6 +12,39 @@ RSpec.describe Url, type: :model do
       is_expected.to validate_presence_of(:short_url)
     end
 
-    # add more tests
+    describe '#perform_click' do
+      let!(:url) { create(:url) }
+
+      it 'creates a click' do
+        expect { url.perform_click('Chrome', 'Linux') }.to change { Click.count }.by(1)
+      end
+
+      it 'updates the click counter' do
+        expect { url.perform_click('Chrome', 'Linux') }.to change { url.clicks_count }.by(1)
+      end
+    end
+
+    def statistics
+      let!(:url) { create(:url) }
+      let(:platform) { 'Linux' }
+      let(:browser) { 'Chrome' }
+      let(:click) { url.perform_click(browser, platform) }
+
+      it 'returns the correct short url' do
+        expect(url.statistics[:short_url]).to eq(url.short_url)
+      end
+
+      it 'returns the correct daily clicks' do
+        expect(url.statistics[:daily_clicks]).to eq([Time.zone.now.day, '1'])
+      end
+
+      it 'returns the correct browser clicks' do
+        expect(url.statistics[:daily_clicks]).to eq([browser, '1'])
+      end
+
+      it 'returns the correct platform clicks' do
+        expect(url.statistics[:daily_clicks]).to eq([platform, '1'])
+      end
+    end
   end
 end
